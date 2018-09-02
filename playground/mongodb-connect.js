7c205070b823d775dd9b1cff0e2014c0d1c97f3f
salujaparam <41211906+salujaparam@users.noreply.github.com>
2018-09-02 11:42:26 +0530

    add post todos route and refactor mongoose

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -7,26 +7,26 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
   }
   console.log('Connected to mongodb server');
 
-  // db.collection('Todos').insertOne({
-  //   text: 'something to do',
-  //   completed: false
-  // }, (err, result) => {
-  //   if(err){
-  //     return console.log('Unable to insert todo', err);
-  //   }
-  //   console.log(JSON.stringify(result.ops, undefined, 2));
-  // });
+  db.collection('Todos').insertOne({
+    text: 'something to do',
+    completed: false
+  }, (err, result) => {
+    if(err){
+      return console.log('Unable to insert todo', err);
+    }
+    console.log(JSON.stringify(result.ops, undefined, 2));
+  });
 
-//   db.collection('Users').insertOne({
-//     name: 'Param',
-//     age: 20,
-//     location: 'Chennai'
-//   }, (err, result) => {
-//     if(err){
-//       return console.log('Unable to insert user', err);
-//     }
-//     console.log(JSON.stringify(result.ops[0]._id.getTimestamp(), undefined, 2));
-//   });
+  db.collection('Users').insertOne({
+    name: 'Param',
+    age: 20,
+    location: 'Chennai'
+  }, (err, result) => {
+    if(err){
+      return console.log('Unable to insert user', err);
+    }
+    console.log(JSON.stringify(result.ops[0]._id.getTimestamp(), undefined, 2));
+  });
 
   db.close();
 });
