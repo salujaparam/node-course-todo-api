1e076a8f8aa09cb12a3d7d3a908ddd958ec3343c
salujaparam <41211906+salujaparam@users.noreply.github.com>
2018-09-03 08:39:21 +0530

    setup app for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -4,8 +4,7 @@ var mongoose = require('mongoose');
 // const dbuser = 'paramSaluja';
 // const dbpassword = 'stoneparam12';
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://<dbuser>:<dbpassword>@ds141952.mlab.com:41952/node-todo-api');
- // || 'mongodb://localhost:27017/TodoApp'
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
 module.exports = {
   mongoose
 };
