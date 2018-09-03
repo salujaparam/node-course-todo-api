86f5d9a88c546e3de37d61d992dcab38bcb7a149
salujaparam <41211906+salujaparam@users.noreply.github.com>
2018-09-03 16:29:50 +0530

    update

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,10 +1,11 @@
 var mongoose = require('mongoose');
 
 
-// const dbuser = 'paramSaluja';
-// const dbpassword = 'stoneparam12';
+const dbuser = 'param';
+const dbpassword = 'stoneparam12';
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost:27017/TodoApp');
+
+mongoose.connect(process.env.DATABASE_URL || 'mongodb://localhost:27017/TodoApp');
 module.exports = {
   mongoose
 };
