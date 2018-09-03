d46a0f17ffd2e6115875328fc546fe71b5fab409
salujaparam <41211906+salujaparam@users.noreply.github.com>
2018-09-03 15:39:32 +0530

    update

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
 var app = express();
-const  port = 41952 || 3000;
+const  port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
