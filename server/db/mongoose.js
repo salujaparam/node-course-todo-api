a6ba2eea7cda3f75ce431c5024539e6e338c828c
salujaparam <41211906+salujaparam@users.noreply.github.com>
2018-09-05 07:06:58 +0530

    setup seperate test and development envs

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -6,7 +6,7 @@ mongoose.Promise = global.Promise;
 
 const uri = "mongodb://param:stoneparam12@ds243502.mlab.com:43502/todoapp";
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONGODB_URI);
 module.exports = {
   mongoose
 };
