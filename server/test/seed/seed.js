3c3f062a5f89181121ee5b1811a6cf23794ae90b
salujaparam <41211906+salujaparam@users.noreply.github.com>
2018-09-08 19:22:08 +0530

    associate users with todos

--- server/test/seed/seed.js
+++ server/test/seed/seed.js
@@ -17,17 +17,23 @@ const users = [{
 }, {
   _id: userTwoId,
   email: 'gen@gmail.com',
-  password: 'userTWo'
+  password: 'userTWo',
+  tokens: [{
+    access: 'auth',
+    token: jwt.sign({_id: userTwoId, acces: 'auth'}, 'abc123').toString()
+  }]
 }]
 
 const todos = [{
   _id: new ObjectID(),
-  text: 'First test todo'
+  text: 'First test todo',
+  _creator: userOneId
 }, {
   _id: new ObjectID(),
   text: 'Second test todo',
   completed: true,
-  completedAt: 333
+  completedAt: 333,
+  _creator: userTwoId
 }];
 
 const populateTodos = (done) => {
